9691c37c066af8a436a9d5c7e4aa168f9f0ace4b
jableaney <124313787+jableaney@users.noreply.github.com>
2023-04-06 19:34:20 -0600

    Add files via upload

--- spacex_dash_app.py
+++ spacex_dash_app.py
@@ -81,7 +81,7 @@ def get_pie_chart(site):
 # TASK 4:
 # Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
 @app.callback(Output(component_id='success-payload-scatter-chart', component_property='figure'),
-            [Input(component_id='site-dropwdown', component_property='value'),
+            [Input(component_id='site-dropdown', component_property='value'),
             Input(component_id='payload-slider', component_property='value')])
 
 def get_scatter_chart(site, payload_range):
